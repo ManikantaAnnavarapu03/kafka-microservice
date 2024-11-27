e8076e51319de1b6e0946fd34d693cd9c5b50637
Manikanta <manikantaannavarapu03@gmail.com>
2024-11-27 12:43:09 +0530

    added order services

--- user-service/controller/controller.js
+++ user-service/controller/controller.js
@@ -7,7 +7,8 @@ const kafka = new Kafka({
 })
 
 async function handleUser(){
-    const consumer = kafka.consumer({groupId:'user-handle-group'})
+    try{
+    const consumer = kafka.consumer({groupId:'user-handle-group', autoCommit:true})
     await consumer.connect()
     console.log("consumer connected")
     await consumer.subscribe({topic:'user-reqq-topic', fromBeginning:true})
@@ -18,13 +19,47 @@ async function handleUser(){
             if(userMessage.action === "login"){
                 var result = ''
                 const data = await db.query('select * from userdata where username = ?', [userMessage.data.username])
+                console.log(data)
+                if(data[0].length == 0){
+                    result = "user not found"
+                }
+                else if(bcrypt.compare(userMessage.data.password, data[0][0].password)){
+                    result = "user valid"
+                }
+                else{
+                    result = "password incorrect"
+                }
                 const producer = kafka.producer({createPartitioner: Partitioners.LegacyPartitioner})
                 await producer.connect()
                 console.log("producer connected")
                 await producer.send({
                     topic:'user-ress-topic',
                     messages:[{
-                        value:JSON.stringify(data)
+                        value:JSON.stringify(result)
+                    }]
+                })
+                await producer.disconnect()
+                console.log("producer disconnected")
+            }
+            else if(userMessage.action == "signup"){
+                const {name, username, password} = userMessage.data
+                const hashPassword = await bcrypt.hash(password, 10)
+                const data = await db.query("insert into userdata values(?, ?, ?)", [name, username, hashPassword])
+                var result = ""
+                if(data.affectedRows >= 1){
+                    result = "user created"
+                }
+                else{
+                    result = "user not created"
+                }
+                console.log(result)
+                const producer = kafka.producer({createPartitioner: Partitioners.LegacyPartitioner})
+                await producer.connect()
+                console.log("producer connected")
+                await producer.send({
+                    topic:'user-ress-topic',
+                    messages:[{
+                        value:JSON.stringify(result)
                     }]
                 })
                 await producer.disconnect()
@@ -32,6 +67,9 @@ async function handleUser(){
             }
         }
     })
+}catch(error){
+    console.log(error)
+}
 }
 
 module.exports = {handleUser}
